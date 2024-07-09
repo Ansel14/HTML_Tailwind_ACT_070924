2d8add501cdea10ad172cfb38b765669d2acea1f
sibayarn <mysibayaneduardandreimaruquez@gmail.com>
2024-07-09 15:55:28 +0800

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".feedback__slider",
+  slideBy: "page",
+  autoplay: true,
+  mouseDrag: true,
+  speed: 400,
+  gutter: 100,
+  items: 1,
+  controls: false,
+  nav: false,
+  autoplayButtonOutput: false,
+});
